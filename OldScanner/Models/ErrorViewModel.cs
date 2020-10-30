f16229a6dd3740e4455120b59aae33bdf8353c28
Aleksandar Jovic <ajovic990@gmail.com>
2020-10-30 07:22:28 +0100

    Init - MVC

---
+++ OldScanner/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace OldScanner.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
